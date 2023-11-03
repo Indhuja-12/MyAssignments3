538abba3b32262f6b6c0284eefa4aacbbe93a248
Indhuja-12 <147424272+Indhuja-12@users.noreply.github.com>
2023-11-03 23:58:16 +0000

    Add files via upload
    Day2

---
+++ day2/WindowAss2.java
@@ -0,0 +1,35 @@
+package week4.day2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.sukgu.Shadow;
+
+public class WindowAss2 {
+
+	public static void main(String[] args) 
+	{
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://login.salesforce.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("hari.radhakrishnan@qeagle.com");
+		driver.findElement(By.xpath("//input[@name='pw']")).sendKeys(" Testleaf@1234 ");	
+		driver.findElement(By.xpath("//input[@name='Login']")).click();
+		//driver.findElement(By.xpath("//span[text()='Learn More']")).click();
+		//Set<String> windowHandles = driver.getWindowHandles();
+		//List<String>lstwindow=new ArrayList<String>(windowHandles);
+		//driver.switchTo().window(lstwindow.get(1));
+		//String title = driver.getTitle();
+		//System.out.println(title);
+		//driver.findElement(By.xpath("//button[text()='Confirm']")).click();
+		//driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
+		//Shadow dom=new Shadow(driver);
+		//dom.findElementByXPath("//span[text()='Products']").click();
+	}
+}
